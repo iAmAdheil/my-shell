e41b8aa7a25bdf2a66793f6e8651dae10ab519aa
ibeginnernoob <adheilgupta@gmail.com>
2026-01-30 22:54:15 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,6 +10,19 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
+func ExtractEchoTxt(s string) string {
+	return s[5:]
+}
+
+func CommandRecog(c string) string {
+	if c == "exit" {
+		return "exit"
+	} else if c[:4] == "echo" {
+		return "echo"
+	}
+	return "not found"
+}
+
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	reader := bufio.NewReader(os.Stdin)
@@ -19,9 +32,16 @@ func main() {
 		// once "enter" is pressed, stops, extracts text before delimiter and clears reader buffer
 		in, _ := reader.ReadString('\n')
 		text := strings.TrimSuffix(in, "\n")
-		if text == "exit" {
+
+		com := CommandRecog(text)
+
+		switch com {
+		case "exit":
 			os.Exit(0)
+		case "echo":
+			fmt.Println(ExtractEchoTxt(text))
+		default:
+			fmt.Println(text + ": command not found")
 		}
-		fmt.Println(text + ": command not found")
 	}
 }
